ab06e9965c8d1e9ced850e58680c367db507402c
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-06-11 21:01:09 +0800

    Add detection result display

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const HAND_LANDMARKS = {
+  0:  'WRIST',
+  1:  'THUMB_CMC',
+  2:  'THUMB_MCP',
+  3:  'THUMB_IP',
+  4:  'THUMB_TIP',
+  5:  'INDEX_FINGER_MCP',
+  6:  'INDEX_FINGER_PIP',
+  7:  'INDEX_FINGER_DIP',
+  8:  'INDEX_FINGER_TIP',
+  9:  'MIDDLE_FINGER_MCP',
+  10: 'MIDDLE_FINGER_PIP',
+  11: 'MIDDLE_FINGER_DIP',
+  12: 'MIDDLE_FINGER_TIP',
+  13: 'RING_FINGER_MCP',
+  14: 'RING_FINGER_PIP',
+  15: 'RING_FINGER_DIP',
+  16: 'RING_FINGER_TIP',
+  17: 'PINKY_MCP',
+  18: 'PINKY_PIP',
+  19: 'PINKY_DIP',
+  20: 'PINKY_TIP'
+};
+
+
 const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
@@ -14,7 +39,7 @@ function onResults(results) {
     canvasCtx.drawImage(
         results.image, 0, 0, canvasElement.width, canvasElement.height
     );
-
+    document.getElementById("input-label").innerHTML = ""
     if (results.multiHandLandmarks) {
         for (const landmarks of results.multiHandLandmarks) {
             drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
@@ -25,6 +50,12 @@ function onResults(results) {
                 color: '#FF0000',
                 lineWidth: 0
             });
+            console.log(landmarks)
+            let landmarkInfo = "";
+            landmarks.forEach(element => {
+                landmarkInfo += `[${element.x.toFixed(2)}, ${element.y.toFixed(2)}, ${element.z.toFixed(2)}]<br/>`
+            });
+            document.getElementById("input-label").innerHTML = landmarkInfo
         }
     }
     canvasCtx.restore();
@@ -37,7 +68,7 @@ const hands = new Hands({
 });
 
 hands.setOptions({
-    maxNumHands: 4,
+    maxNumHands: 1,
     modelComplexity: 1,
     minDetectionConfidence: 0.5,
     minTrackingConfidence: 0.5
